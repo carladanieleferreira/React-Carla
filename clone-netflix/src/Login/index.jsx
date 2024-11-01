b4ca5fda2d4b3f31a0868c6beddbe7678b10fd39
Carla Daniele Ferreira <daniele.fersan@gmail.com>
2024-11-01 17:22:36 -0300

    criação Login

---
+++ clone-netflix/src/Login/index.jsx
@@ -0,0 +1,14 @@
+import { } from "./style";
+
+export const Login = () => {
+
+    return(
+            <body>
+                <h1>oi</h1>
+                <img src ={"https://i.imgur.com/trQAZQE.jpeg"}/>
+                https://i.imgur.com/trQAZQE.jpeg
+                
+            </body>
+
+    )
+}
---
+++ clone-netflix/src/Login/style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+`;
+
+export const Bodyfilmes = styled.div`
+ background-image: url(assets/netflix-background.jpg);
+`;
+
+export const Filme = styled.div`
+`;
